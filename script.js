8e717036ce20861e28f8af2226fa14ea0c802f3e
BonesTheGhost <bonestheghost@gmail.com>
2020-07-21 12:13:57 -0500

    [004-(0.0.7)]:: Tons of Learning! >> Learned how to store 'querySelector' as an arrow function, to elimiante need for redundant 'getElementById' calls. Applied an 'onclick' event to the hamburger menu (via class parameter) in said 'querySelector' function. Then, used 'classList' to toggle '.active' CSS styling to the menu icons. Furthermore, hooked up the CSS styling to add in the backdrop upon clicking the burger menu icons. Very efficient JS code here! I really respect this CSS driven approach, with just enough JS to animate the thoughtful design.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("[script.js]:: Connected and working properly!");
+
+//document.querySelector returns the first element within the document that matches the specified html 'selector' or '#id' or '.class'.
+//Here it is stored as an arrow function, meaning it can be called repeatedly.
+const selectElement = (element) => document.querySelector(element);
+
+//Here, we simply say 'go and find ('element') and return it into the variable.
+const header = selectElement('header');
+const mainContent = selectElement('main');
+
+//Find the element with class of '.hamburger', add an onclick event, and toggle active.
+//classList is a DOMListObject, where we can add() or remove() classes to.
+//toggle: As opposed to manually typing each class every time add/remove('nav-link myStyle active')
+selectElement('.hamburger').addEventListener('click', () => {
+  header.classList.toggle('active');
+});
+
